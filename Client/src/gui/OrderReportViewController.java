61f6dfb43139c964c4e43fa1e704f54ab3f09b46
AsafSchneiderman <aassaaff323@gmail.com>
2022-12-26 13:08:35 +0200

    Updated OrderReportView files (fxml and controller)

---
+++ Client/src/gui/OrderReportViewController.java
@@ -0,0 +1,46 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import Entities.Report;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class OrderReportViewController implements Initializable{
+
+    @FXML
+    private BarChart<?, ?> BarChartForOrdersPerProductsMachine;
+
+    @FXML
+    private Button BntBack;
+
+    @FXML
+    private CategoryAxis categoryAxisInstitutions;
+
+    @FXML
+    private NumberAxis numberAxisSales;
+
+    @FXML
+    void BackToPreviosePage(ActionEvent event) {
+
+    }
+
+	public void start(Stage clientStage, Report firstReport) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
