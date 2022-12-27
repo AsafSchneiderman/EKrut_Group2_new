edca680726e718b6c802f69d8f9ccd2eb06dda21
USER <USER@DESKTOP-KMG62J9>
2022-12-27 17:04:29 +0200

    added CustomerRegistrationController

---
+++ Client/src/gui/CustomerRegistrationController.java
@@ -0,0 +1,49 @@
+package gui;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+public class CustomerRegistrationController {
+
+	    @FXML
+	    private Label registrationFormForCustomerLbl;
+
+	    @FXML
+	    private Label FirstNameCustomerRegistrationLbl;
+
+	    @FXML
+	    private Label lastNameCustomerRegistrationLbl;
+
+	    @FXML
+	    private Label IDNumberCustomerRegistrationLbl;
+
+	    @FXML
+	    private Label emailCustomerRegistrationLbl;
+
+	    @FXML
+	    private Label phoneCustomerRegistrationLbl;
+
+	    @FXML
+	    private TextField furstNameCustomerRegistrationTxt;
+
+	    @FXML
+	    private TextField lastNameCustomerRegistartionTxt;
+
+	    @FXML
+	    private TextField idCustomerRegistrationTxt;
+
+	    @FXML
+	    private TextField emailCustomerRegistrationTxt;
+
+	    @FXML
+	    private TextField phoneCustomerRegistartionTxt;
+
+	    @FXML
+	    private Button senCustomerRegistrationBtn;
+
+	    @FXML
+	    private Button backCustomerRegistarionBtn;
+
+	
+
+}
