1b82c3a57c6d0c265e54005da99d6f7089d59213
USER <USER@DESKTOP-KMG62J9>
2022-12-28 12:01:28 +0200

    customer service page fxml and controller

---
+++ Client/src/gui/CustomerServiceController.java
@@ -0,0 +1,24 @@
+package gui;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class CustomerServiceController {
+	
+	
+
+	    @FXML
+	    private Label customerServicelbl;
+
+	    @FXML
+	    private Button registerCustomerBtn;
+
+	    @FXML
+	    private Button registerEmployeeBtn;
+
+	    @FXML
+	    private Button logoutCustomerServiceBtn;
+
+	
+
+}
