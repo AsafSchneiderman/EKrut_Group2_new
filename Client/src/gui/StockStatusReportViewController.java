77569727be0ea82e8cb872572bc4c80351576edb
AsafSchneiderman <aassaaff323@gmail.com>
2022-12-29 16:03:04 +0200

    I updated the stock status report view from menu button to choice box
    updated window size

--- Client/src/gui/StockStatusReportViewController.java
+++ Client/src/gui/StockStatusReportViewController.java
@@ -1,54 +1,59 @@
 package gui;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import Entities.Report;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart.Series;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.MenuButton;
 import javafx.stage.Stage;
 
-public class StockStatusReportViewController implements Initializable{
+public class StockStatusReportViewController implements Initializable {
 
-    @FXML
-    private BarChart<?, ?> BarChartStockByMachine;
+	@FXML
+	private BarChart<String, Integer> BarChartStockPerMachine;
 
-    @FXML
-    private Button BntBack;
+	@FXML
+	private CategoryAxis categoryAxisProduct;
 
-    @FXML
-    private MenuButton BntChooseMachine;
+	@FXML
+	private ChoiceBox<String> cbInstitution;
 
-    @FXML
-    private CategoryAxis categoryAxisProduct;
+	@FXML
+	private NumberAxis numberAxisAmount;
+	
+	public static ObservableList<String> institutionList = FXCollections.observableArrayList();
+	private static Series<String, Integer> series;
+	
+	@FXML
+	void BackToPreviosePage(ActionEvent event) {
 
-    @FXML
-    private NumberAxis numberAxisAmount;
+	}
 
-    @FXML
-    void BackToPreviosePage(ActionEvent event) {
 
-    }
+	public void start(Stage clientStage, List<Report> selectedReports) {
 
-    @FXML
-    void chooseMachine(ActionEvent event) {
-
-    }
-
-	public void start(Stage clientStage, Report[] selectedReports) {
-		
-		
 	}
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
+
+	}
+	
+	/** sets the data of the barChart**/
+	public void setStockPerMachineData() {
 		
 	}
 
