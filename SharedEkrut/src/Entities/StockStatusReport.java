9d8afa25c6322389312f97008168e58409fd4eff
AsafSchneiderman <aassaaff323@gmail.com>
2022-12-30 20:05:50 +0200

    updated product array to VendingMachine list

--- SharedEkrut/src/Entities/StockStatusReport.java
+++ SharedEkrut/src/Entities/StockStatusReport.java
@@ -1,16 +1,19 @@
 package Entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StockStatusReport extends Report {
 	private static final long serialVersionUID = 3804980031254229265L;
-	private Product[] products;
+	private List<VendingMachine> vendingMachines;
 	
-	public StockStatusReport(String month, String year,Product[] products) {
+	public StockStatusReport(String month, String year,ArrayList<VendingMachine> vendingMachines) {
 		super("Stock status", month, year);
-		this.products = products;
+		this.vendingMachines = vendingMachines;
 	}
 
-	public Product[] getProducts() {
-		return products;
+	public List<VendingMachine> getVendingMachines() {
+		return vendingMachines;
 	}
 
 }
