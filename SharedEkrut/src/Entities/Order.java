0f6d7cfb371c32a43ccf1b8b9f66a2d37505b117
AsafSchneiderman <aassaaff323@gmail.com>
2023-01-03 20:30:40 +0200

    updated the Order entity(i removed the vending machine id)

--- SharedEkrut/src/Entities/Order.java
+++ SharedEkrut/src/Entities/Order.java
@@ -4,18 +4,17 @@ import java.util.ArrayList;
 public class Order implements Serializable{
 	private static final long serialVersionUID = -8005643298429981017L;
 	private int orderNum;
-	private String orderDate, orderStatus, customerID, vendingMachineLocation, vendingMachineID,orderType;
+	private String orderDate, orderStatus, customerID, vendingMachineLocation,orderType;
 	private float totalPrice;
 	private String products;
 	private int quantityOfProducts;
 	int [] quantityPerProducts;
 	
-	public Order (String vendingMachineLocation,String orderDate,String orderStatus,String customerID,String vendingMachineID,float totalPrice, String orderType, int quantityOfProducts) {
+	public Order (String vendingMachineLocation,String orderDate,String orderStatus,String customerID,float totalPrice, String orderType, int quantityOfProducts) {
 		this.vendingMachineLocation=vendingMachineLocation;
 		this.orderDate=orderDate;
 		this.orderStatus=orderStatus;
 		this.customerID=customerID;
-		this.vendingMachineID=vendingMachineID;
 		this.totalPrice=totalPrice;
 		this.orderType=orderType;
 		this.quantityOfProducts = quantityOfProducts;
@@ -61,12 +60,8 @@ public class Order implements Serializable{
 	public void setCustomerID(String customerID) {
 		this.customerID=customerID;
 	}
-	public String getVendingMachineID() {
-		return vendingMachineID;
-	}
-	public void setVendingMachineID(String vendkingMachineID) {
-		this.vendingMachineID=vendingMachineID;
-	}
+	
+	
 	public float getTotalPrice() {
 		return totalPrice;
 	}
