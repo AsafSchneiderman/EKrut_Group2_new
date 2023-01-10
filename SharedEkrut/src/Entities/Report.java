54f6cc2d1348ca5de9f62a5466d1b4c74c8740bc
AsafSchneiderman <aassaaff323@gmail.com>
2023-01-10 17:34:28 +0200

    Updated to ReportType variable

--- SharedEkrut/src/Entities/Report.java
+++ SharedEkrut/src/Entities/Report.java
@@ -6,17 +6,17 @@ public class Report implements Serializable{
 	
 	private static final long serialVersionUID = 583542510006825068L;
 	
-	private String reportType;
+	private ReportType reportType;
 	private String month;
 	private String year;
 	
-	public Report(String reportType, String month, String year) {
+	public Report(ReportType reportType, String month, String year) {
 		this.reportType = reportType;
 		this.month = month;
 		this.year = year;
 	}
 	
-	public String getReportType() {
+	public ReportType getReportType() {
 		return reportType;
 	}
 	public String getMonth() {
@@ -25,4 +25,5 @@ public class Report implements Serializable{
 	public String getYear() {
 		return year;
 	}
+	
 }
