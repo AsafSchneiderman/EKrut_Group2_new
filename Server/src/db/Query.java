fa3b33b98ea6355a96f1d12cc4f42bad992e5ac4
AsafSchneiderman <aassaaff323@gmail.com>
2023-01-10 17:40:45 +0200

    Updated getReports to use the current input form
    Extracted query for orders report to 'getOrdersByDateAndRegion' function
    
    Extracted query for stock report to 'getProductStockByDateAndMachine'
    function
    
    Extracted query for clientActivity report to
    'getClientActivityByDateAndMachine'

--- Server/src/db/Query.java
+++ Server/src/db/Query.java
@@ -182,7 +182,7 @@ public class Query {
 	}
 
 	/**
-	 * update the Restock Status of the vending machine in location in the DB.
+	 * update the Re-stock Status of the vending machine in location in the DB.
 	 * 
 	 * @param vendingmachines list to update them threshold level
 	 */
@@ -210,107 +210,169 @@ public class Query {
 	 * 
 	 * @return ArrayList of reports from the DB
 	 */
-	public static ArrayList<Report> getReports(String region) {
+	public static ArrayList<Report> getReports(String input) {
+		String[] inputArray = input.split("\\#");
+		ReportType reportType;
+		String month = inputArray[1];
+		String year = inputArray[2];
+		String region = inputArray[3];
 		ArrayList<Report> reports = new ArrayList<>();
-
 		Statement stmt;
 		try {
 			if (mysqlConnection.conn != null) {
 				stmt = mysqlConnection.conn.createStatement();
-				ResultSet rs = stmt.executeQuery("SELECT * FROM reports");
+				ResultSet rs;
+				if (inputArray[0].equals("Show all report types"))
+					rs = stmt.executeQuery("SELECT * FROM reports");
+				else 
+					rs = stmt.executeQuery("SELECT * FROM reports WHERE report_type = \""+ inputArray[0] + "\"");
+							
+				ArrayList<VendingMachine> vendingMachines = getVendingMachines();
+				
 				while (rs.next()) {
 					Report r = null;
-					if (rs.getString("report_type") == "order") {
-						ArrayList<Order> orders = new ArrayList<>();
-
-						ResultSet rs2 = stmt.executeQuery("SELECT orders.*, vendingmachines.region from orders"
-								+ " inner join vendingmachines on orders.machineLocation=vendingmachines.location"
-								+ " WHERE MONTH(orderDate)=" + rs.getString("month") + " AND YEAR(orderDate)="
-								+ rs.getString("year") + " AND region=\"" + region + "\"");
-						while (rs2.next()) {
-
-							Order o = new Order(rs2.getString("machineLocation"), rs2.getString("date"),
-									rs2.getString("status"), rs2.getString("customerID"), rs2.getFloat("totPrice"),
-									rs2.getString("type"), rs2.getInt("productsQauntity"));
-							orders.add(o);
-
-						}
-						r = new OrdersReport(rs.getString("month"), rs.getString("year"), orders);
-					} else if (rs.getString("report_type") == "stockStatus") {
-
+					reportType = ReportType.valueOf(rs.getString("report_type"));
+					switch(reportType) {
+					case Order:
+						ArrayList<Order> orders = getOrdersByDateAndRegion(month, year, region);
+						r = new OrdersReport(month, year, orders);
+						break;
+					case Stock_Status:
 						ArrayList<ArrayList<Product>> stocks = new ArrayList<>();
-						ArrayList<VendingMachine> vendingMachines = getVendingMachines();
+						ArrayList<VendingMachine> vendingMachinesInRegion = new ArrayList<>();
 						for (VendingMachine v : vendingMachines) {
 							if (v.getRegion().equals(region)) {
 								String currentMachine = v.getLocation().toLowerCase();
-								String currentStock = currentMachine + "products";
-								ResultSet rs2 = stmt.executeQuery("SELECT * FROM " + currentStock);
-								ArrayList<Product> productsStock = new ArrayList<>();
-
-								ResultSet rs3 = stmt
-										.executeQuery("SELECT * FROM db_ekrut.orders WHERE (orderDate BETWEEN \' "
-												+ rs.getString("year") + "-" + rs.getString("month")
-												+ "-01\' AND CURDATE() ) AND ( machineLocation = " + currentMachine
-												+ ")");
-								while (rs2.next()) {
-									String[] productsIDs = rs3.getString("products").split("\\,");
-									String[] productsQuantities = rs3.getString("QuantityPerProduct").split("\\,");
-									int[] removedStocks = new int[30];
-									for (int i = 0; i < 30; i++)
-										removedStocks[i] = 0;
-
-									for (int i = 0; i < rs3.getInt("productsQuantity"); i++) {
-										int productID = Integer.parseInt(productsIDs[i]);
-										int productQuantity = Integer.parseInt(productsQuantities[i]);
-										removedStocks[productID] = removedStocks[productID] + productQuantity;
-
-									}
-									int ID = Integer.parseInt(rs2.getString("productID"));
-									int quantity = rs2.getInt("stockQuantity") + removedStocks[ID];
-									Product product = new Product(rs2.getString("productName"),
-											rs2.getString("productID"), rs2.getString("price"), quantity + "", null);
-									productsStock.add(product);
-								}
-
+								ArrayList<Product> productsStock = getProductStockByDateAndMachine(month, year, currentMachine);
+								vendingMachinesInRegion.add(v);
 								stocks.add(productsStock);
 							}
 						}
-						r = new StockStatusReport(rs.getString("month"), rs.getString("year"), vendingMachines, stocks);
-					} else if (rs.getString("report_type") == "clientActivity") {
-						ArrayList<VendingMachine> vendingMachines = getVendingMachines();
+						r = new StockStatusReport(month, year, vendingMachinesInRegion, stocks);
+						break;
+					case Client_Activity:
 						ArrayList<HashMap<Integer,Integer>> clientsActivityPerMachine=new ArrayList<>();
 						
 						for (VendingMachine v : vendingMachines)
 						{
 							if(v.getRegion().equals(region))
 							{
-								HashMap<Integer,Integer> clientActivity= new HashMap<>();
-								ResultSet rs2 = stmt.executeQuery("SELECT orders.productsQuantity,  COUNT(*) AS clientsAmount from orders" 
-										+" inner join vendingmachines on orders.machineLocation=vendingmachines.location"
-										+" WHERE MONTH(orderDate)= "+rs.getString("month")+" AND YEAR(orderDate)= "+rs.getString("year")+" AND machineLocation= "+v.getLocation() 
-										+" group by productsQuantity");
-								while(rs.next()) {
-									clientActivity.put(rs2.getInt("productsQuantity"), rs2.getInt("clientsAmount"));
-								}
-								
-								clientsActivityPerMachine.add(clientActivity);
+								HashMap<Integer, Integer> clientsPerOrderAmount = getClientActivityByDateAndMachine(month, year, v);
+								clientsActivityPerMachine.add(clientsPerOrderAmount);
 							}
-							
 						}
-						r = new ClientActivityReport(rs.getString("month"), rs.getString("year"), clientsActivityPerMachine);
+						r = new ClientActivityReport(month, year, clientsActivityPerMachine);
+						break;
+						
+					default:
+						break;
 					}
 					reports.add(r);
 				}
-				rs.close();
-			} else {
-				System.out.println("Conn is null");
-			}
+				rs.close();	
+			} 
+			else System.out.println("Conn is null");
 
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 
 		return reports;
+	}
+
+	/**
+	 * @param month month of order
+	 * @param year year of order
+	 * @param region of order
+	 * @return ArrayList of orders
+	 */
+	private static ArrayList<Order> getOrdersByDateAndRegion(String month, String year, String region){
+		ArrayList<Order> orders = new ArrayList<>();
+		try {
+			Statement stmt = mysqlConnection.conn.createStatement();
+			
+			ResultSet rs = stmt.executeQuery("SELECT orders.*, vendingmachines.region from orders"
+					+ " inner join vendingmachines on orders.machineLocation=vendingmachines.location"
+					+ " WHERE MONTH(orderDate)=" + month + " AND YEAR(orderDate)=" + year + " AND region=\"" + region + "\"");
+			while (rs.next()) {
+	
+				Order o = new Order(rs.getString("machineLocation"), rs.getString("date"),
+						rs.getString("status"), rs.getString("customerID"), rs.getFloat("totPrice"),
+						rs.getString("type"), rs.getInt("productsQauntity"));
+				orders.add(o);
+	
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return orders;
+	}
+
+	/**
+	 * 
+	 * @param month month of stock status
+	 * @param year year of stock status
+	 * @param currentMachine the machine associated with the stock status
+	 * @return
+	 */
+	private static ArrayList<Product> getProductStockByDateAndMachine(String month, String year, String currentMachine) {
+		ArrayList<Product> productsStock = new ArrayList<>();
+		try {
+			Statement stmt = mysqlConnection.conn.createStatement();
+			String currentStock = currentMachine + "products";
+			ResultSet rs = stmt.executeQuery("SELECT * FROM " + currentStock);	
+			ResultSet rs2 = stmt.executeQuery("SELECT * FROM db_ekrut.orders WHERE (orderDate BETWEEN \' "
+							+ year + "-" + month + "-01\' AND CURDATE() ) AND ( machineLocation = " + currentMachine + ")");
+			while (rs.next()) {
+				String[] productsIDs = rs2.getString("products").split("\\,");
+				String[] productsQuantities = rs2.getString("QuantityPerProduct").split("\\,");
+				int[] removedStocks = new int[30];
+				for (int i = 0; i < 30; i++)
+					removedStocks[i] = 0;
+	
+				for (int i = 0; i < rs2.getInt("productsQuantity"); i++) {
+					int productID = Integer.parseInt(productsIDs[i]);
+					int productQuantity = Integer.parseInt(productsQuantities[i]);
+					removedStocks[productID] = removedStocks[productID] + productQuantity;
+	
+				}
+				int ID = Integer.parseInt(rs.getString("productID"));
+				int quantity = rs.getInt("stockQuantity") + removedStocks[ID];
+				Product product = new Product(rs.getString("productName"),
+						rs.getString("productID"), rs.getString("price"), quantity + "", null);
+				productsStock.add(product);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return productsStock;
+	}
+
+	
+	/**
+	 * 
+	 * @param month month of checked client activity
+	 * @param year year of checked client activity
+	 * @param v the vending machine of the client activity
+	 * @return
+	 */
+	private static HashMap<Integer, Integer> getClientActivityByDateAndMachine(String month, String year, VendingMachine v) {
+		HashMap<Integer,Integer> clientsPerOrderAmount= new HashMap<>();
+		try {
+			Statement stmt = mysqlConnection.conn.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT OrdersPerClient.OrderAmount, COUNT(*) AS ClientAmount FROM (SELECT customerID, COUNT(*) AS OrderAmount FROM orders"
+					+" inner join vendingmachines on orders.machineLocation=vendingmachines.location"
+					+" WHERE MONTH(orderDate)= "+ month +" AND YEAR(orderDate)= "+year+" AND machineLocation= \""+v.getLocation()+"\""
+					+" GROUP BY customerID) AS OrdersPerClient"
+					+" GROUP BY OrdersPerClient.OrderAmount"
+					+" ORDER BY OrdersPerClient.OrderAmount");
+			while(rs.next())
+				clientsPerOrderAmount.put(rs.getInt("OrderAmount"), rs.getInt("ClientAmount"));
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return clientsPerOrderAmount;
 	}
 
 	public static ArrayList<Product> getProducts() {
@@ -361,27 +423,5 @@ public class Query {
 		return "noRegion";
 	}
 
-	/**
-	 * get vending machine locations by name of region
-	 * 
-	 * @param regionName the name of the region
-	 * @return vending machine locations
-	 */
-	public static ArrayList<String> getLocations(String regionName) {
-		ArrayList<String> locations = new ArrayList<>();
-		Statement stmt;
-		try {
-			if (mysqlConnection.conn != null) {
-				stmt = mysqlConnection.conn.createStatement();
-				ResultSet rs = stmt
-						.executeQuery("SELECT location FROM vendingmachines WHERE region =\"" + regionName + "\"");
-				while (rs.next()) {
-					locations.add(rs.getString("location"));
-				}
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return locations;
-	}
+	
 }
