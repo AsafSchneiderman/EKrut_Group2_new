0a73b258ceb88ca1289ad5e4705203995ef8c178
AsafSchneiderman <116450712+AsafSchneiderman@users.noreply.github.com>
2023-01-11 18:53:16 +0200

    Add files via upload

---
+++ src/gui/StockStatusReportController.java
@@ -0,0 +1,29 @@
+package gui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
+
+public class StockStatusReportController {
+
+    @FXML
+    private BarChart<?, ?> BarChartStockByMachine;
+
+    @FXML
+    private MenuButton BntChooseMachine;
+
+    @FXML
+    private Button BntBack;
+
+    @FXML
+    void BackToPreviosePage(ActionEvent event) {
+
+    }
+
+    @FXML
+    void chooseMachine(ActionEvent event) {
+
+    }
+}
