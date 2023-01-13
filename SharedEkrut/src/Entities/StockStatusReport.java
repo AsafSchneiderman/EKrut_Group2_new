440b51a468fd1ae53fb0acdf84a3e171d110369c
AsafSchneiderman <aassaaff323@gmail.com>
2023-01-13 23:57:52 +0200

    added getGraph function for creating reports

--- SharedEkrut/src/Entities/StockStatusReport.java
+++ SharedEkrut/src/Entities/StockStatusReport.java
@@ -3,6 +3,9 @@ package Entities;
 import java.util.ArrayList;
 import java.util.List;
 
+import javafx.scene.chart.XYChart;
+import javafx.scene.chart.XYChart.Series;
+
 public class StockStatusReport extends Report {
 	private static final long serialVersionUID = 3804980031254229265L;
 	private List<VendingMachine> vendingMachines;
@@ -20,6 +23,69 @@ public class StockStatusReport extends Report {
 	
 	public ArrayList<Product> getStocks(int index){
 		return stocks.get(index);
+	}
+	
+	public String toString() {
+		String res = null;
+		for (int i = 0 ; i < vendingMachines.size(); i++)
+			res += "month: "+this.getMonth()+", year: "+this.getYear()
+				+ "\nMachineLocation: " + vendingMachines.get(i).getLocation() + "\nstocks:\n[" +stocks.get(i).toString() + "]";
+		return res + "\n";
+	}
+	
+	
+	/**
+	 * 
+	 * @return List of the names of all the vending machine locations
+	 */
+	public List<String> getVendingMachinesLocations(){
+		List<String> locations = new ArrayList<>();
+		for (VendingMachine vendingMachine : vendingMachines)
+			locations.add(vendingMachine.getLocation());
+		return locations;
+	}
+	
+	/**
+	 * 
+	 * @param location the location of the vending machine
+	 * @return List of the names of all the products in the vending machine
+	 */
+	public List<String> getNameOfProducts(String location) {
+		List<String> productsNames = new ArrayList<>();
+		List<String> names = getVendingMachinesLocations();
+		ArrayList<Product> products = getStocks(names.indexOf(location));
+		for (Product product : products)
+			productsNames.add(product.getProductName());
+		return productsNames;
+	}
+	
+	/**
+	 * 
+	 * @param location the location of the vending machine
+	 * @return List of the stock quantity of each product in the vending machine
+	 */
+	public List<Integer> getQuantityOfPorducts(String location){
+		List<Integer> productsStockQuantity = new ArrayList<>();
+		List<String> names = getVendingMachinesLocations();
+		ArrayList<Product> products = stocks.get(names.indexOf(location));
+		for (Product product : products)
+			productsStockQuantity.add(Integer.parseInt(product.getStockQuantity()));
+		return productsStockQuantity;
+		
+	}
+
+	/**
+	 * 
+	 * @param location the location of the vending machine
+	 * @return Series of quantities per product in the vending machine
+	 */
+	public Series<String, Integer> getGraph(String location) {
+		XYChart.Series<String, Integer> series = new XYChart.Series<>();
+		List<String> productsNames = getNameOfProducts(location);
+		List<Integer> productsStockQuantity = getQuantityOfPorducts(location);
+		for (int i = 0 ; i < productsNames.size() ; i++)
+			series.getData().add(new XYChart.Data<String, Integer>(productsNames.get(i), productsStockQuantity.get(i)));
+		return series;
 	}
 
 }
